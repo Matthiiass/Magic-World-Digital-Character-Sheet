f4179231283d643668619ef5dfa7ac2bbba21f76
Matthiiass <matthewgregory278@gmail.com>
2023-01-06 09:18:45 +0000

    More spell progress

--- magics.js
+++ magics.js
@@ -19,4 +19,76 @@ spellList = {
     "light": {
         "Beam of Light": 1
     },
+    "dark": {
+        "Dark Pulse": 1
+    },
+    "smoke": {
+        "Smokescreen": 1
+    },
+    "lightning": {
+        "Strikedown": 1
+    },
+    "ice": {
+        "Frosty Wind": 1
+    },
+    "nature": {
+        'Leaf Blast': 1
+    },
+    "chaos": {
+        'Erratic Pulse': 1
+    },
+    "order": {
+        "Follow Shot": 1
+    },
+    "love": {
+        "Charm": 1
+    },
+    "explosion": {
+        "Explosive Round": 1
+    },
+    "shadow": {
+        "Shadow Ball": 1
+    },
+    "aurora": {
+        "Dazzle": 1
+    },
+    "sound": {
+        "Piercing Screech": 1
+    },
+    "illusion": {
+        "Tri-Shot": 1
+    },
+    "dream": {
+        "Infiltrator": 1
+    },
+    "nightmare": {
+        "Dream Eater": 1
+    },
+    "spatial": {
+        "Store": 1
+    },
+    "gravity": {
+        "Drag-Down": 1
+    },
+    "life": {
+        "Bless": 1
+    },
+    "time": {
+        "Future Strike": 1
+    },
+    "space": {
+        "Meator Rain": 1
+    },
+    "death": {
+        "Awaken": 1
+    },
+    "fairy": {
+        "Dance of the Fairies": 1
+    },
+    "ghost": {
+        "Ghost Step": 1
+    },
+    "antimagic": {
+        "Barrier": 1
+    }
 }
